ce28cd4dcaf7b1c859b26428854524997ffba999
debbieAmoni <debbieamoni7@gmail.com>
2026-06-18 23:13:23 +0000

    feat: add constants file for network config and delays

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const STELLAR_NETWORK = process.env.REACT_APP_STELLAR_NETWORK ?? 'testnet';
+export const SOROBAN_RPC = 'https://soroban-testnet.stellar.org';
+export const PROOF_MOCK_DELAY_MS = 3000;
+export const CONFIRM_MOCK_DELAY_MS = 2000;
